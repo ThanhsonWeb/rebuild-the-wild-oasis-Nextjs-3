f30a0595d1515fc00f1dddf94549728c69e4f8a5
ThanhsonWeb <sondeptroi@gmail.com>
2026-07-19 09:56:12 +0700

    Display  user session

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
+import Image from "next/image";
 
-function Navigation() {
+async function Navigation() {
+	const session = await auth();
+
 	return (
 		<nav className="z-10 text-xl">
 			<ul className="flex gap-16 items-center">
@@ -20,14 +24,34 @@ function Navigation() {
 						About
 					</Link>
 				</li>
-				<li>
-					<Link
-						href="/account"
-						className="hover:text-accent-400 transition-colors"
-					>
-						Guest Area
-					</Link>
-				</li>
+				{session ? (
+					<div className="flex items-center gap-3">
+						<Image
+							src={session?.user?.image}
+							width={30}
+							height={30}
+							className="rounded-xl"
+							alt="user's image"
+						/>
+						<li>
+							<Link
+								href="/account"
+								className="hover:text-accent-400 transition-colors"
+							>
+								Guest Area
+							</Link>
+						</li>
+					</div>
+				) : (
+					<li>
+						<Link
+							href="/account"
+							className="hover:text-accent-400 transition-colors"
+						>
+							Guest Area
+						</Link>
+					</li>
+				)}
 			</ul>
 		</nav>
 	);
